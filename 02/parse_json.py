a96fb330563d8da330bb85299608f151bae7e804
maxshilin <max.shilin@mail.ru>
2022-10-05 21:34:15 +0300

    Reduced asymptotic complexity of the algorithm, as well as reduced nesting

--- 02/parse_json.py
+++ 02/parse_json.py
@@ -12,25 +12,34 @@ def get_tokens(field):
     return None
 
 
+def add_to_dict(token, num, found_keywords):
+    if token not in found_keywords:
+        found_keywords[token] = num
+    else:
+        found_keywords[token] += num
+
+
 def parse_json(
     json_str: str, keyword_callback, required_fields=None, keywords=None
 ):
     if not required_fields or not keywords or not json_str:
         return None
+    set_keywords = set(keywords)
     found_keywords = {}
     parsed_json = json.loads(json_str)
 
     for field in required_fields:
-        if field in parsed_json:
-            tokens = get_tokens(parsed_json[field])
-        else:
+        if field not in parsed_json:
             continue
-        if tokens:
-            for token in tokens:
-                if token in keywords:
-                    keyword_callback(token)
-                    if token not in found_keywords:
-                        found_keywords[token] = 1
-                    else:
-                        found_keywords[token] += 1
+
+        tokens = get_tokens(parsed_json[field])
+        if not tokens:
+            continue
+
+        matched_tokens = set(tokens) & set_keywords
+        for token in matched_tokens:
+            num = tokens.count(token)
+            for _ in range(num):
+                keyword_callback(token)
+            add_to_dict(token, num, found_keywords)
     return found_keywords
