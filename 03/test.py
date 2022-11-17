8969e1788285b5f5c5be6f4f2bb53c3181f66485
maxshilin <max.shilin@mail.ru>
2022-11-17 21:01:45 +0300

    Added new tests in test.py

--- 03/test.py
+++ 03/test.py
@@ -7,15 +7,15 @@ class TestCustomList(unittest.TestCase):
     def test_list_methods(self):
         custom_list = CustomList([])
         custom_list.append(1)
-        self.assertEqual(custom_list, CustomList([1]))
+        self.assertListEqual(custom_list, CustomList([1]))
 
         custom_list = CustomList([1, 2, 3])
         custom_list2 = custom_list.copy()
-        self.assertEqual(custom_list2[0:], CustomList([1, 2, 3])[0:])
+        self.assertListEqual(custom_list2, CustomList([1, 2, 3]))
 
         custom_list2.clear()
-        self.assertEqual(custom_list2, [])
-        self.assertEqual(custom_list[0:], CustomList([1, 2, 3])[0:])
+        self.assertListEqual(custom_list2, [])
+        self.assertListEqual(custom_list, CustomList([1, 2, 3]))
 
         custom_list = CustomList([1, 2, 3, 2, 4, 2, 6, 4])
         self.assertEqual(custom_list.count(2), 3)
@@ -23,144 +23,178 @@ class TestCustomList(unittest.TestCase):
 
         custom_list = CustomList([1, 2, 3, 2, 4])
         custom_list.extend([1, 2])
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([1, 2, 3, 2, 4, 1, 2])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([1, 2, 3, 2, 4, 1, 2]),
         )
 
         custom_list = CustomList([1, 2, 3, 6, 4])
         custom_list.remove(6)
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([1, 2, 3, 4])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([1, 2, 3, 4]),
         )
 
         custom_list = CustomList([1, 2, 3, 6, 4])
         custom_list.pop(2)
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([1, 2, 6, 4])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([1, 2, 6, 4]),
         )
 
         custom_list = CustomList([1, 2, 3, 6, 4])
         custom_list.insert(3, 121)
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([1, 2, 3, 121, 6, 4])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([1, 2, 3, 121, 6, 4]),
         )
 
         custom_list = CustomList([1, 23, 4, 21.4, -3])
         custom_list.reverse()
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([-3, 21.4, 4, 23, 1])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([-3, 21.4, 4, 23, 1]),
         )
 
         custom_list.sort()
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([-3, 1, 4, 21.4, 23])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([-3, 1, 4, 21.4, 23]),
         )
 
         custom_list.sort(reverse=True)
-        self.assertEqual(
-            custom_list[0:],
-            CustomList([23, 21.4, 4, 1, -3])[0:],
+        self.assertListEqual(
+            custom_list,
+            CustomList([23, 21.4, 4, 1, -3]),
         )
 
     def test_addition(self):
         custom_list1 = CustomList([])
         custom_list2 = CustomList([])
         custom_list3 = CustomList([])
-        self.assertEqual(custom_list1 + custom_list2, custom_list3)
+        self.assertListEqual(custom_list1 + custom_list2, custom_list3)
+        self.assertListEqual(custom_list1, CustomList([]))
+        self.assertListEqual(custom_list2, CustomList([]))
 
         custom_list1 = CustomList([1, 2])
         custom_list2 = CustomList([])
         custom_list3 = CustomList([1, 2])
-        self.assertEqual((custom_list1 + custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 + custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 2]))
+        self.assertListEqual(custom_list2, CustomList([]))
 
         custom_list1 = CustomList([])
         custom_list2 = CustomList([3, 4])
         custom_list3 = CustomList([3, 4])
-        self.assertEqual((custom_list1 + custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 + custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([]))
+        self.assertListEqual(custom_list2, CustomList([3, 4]))
 
         custom_list1 = CustomList([1, 2, 3, 4])
         custom_list2 = CustomList([1, 9])
         custom_list3 = CustomList([2, 11, 3, 4])
-        self.assertEqual((custom_list1 + custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 + custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 2, 3, 4]))
+        self.assertListEqual(custom_list2, CustomList([1, 9]))
 
         custom_list1 = CustomList([1, 9])
         custom_list2 = CustomList([1, 2, 3, 4])
         custom_list3 = CustomList([2, 11, 3, 4])
-        self.assertEqual((custom_list1 + custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 + custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 9]))
+        self.assertListEqual(custom_list2, CustomList([1, 2, 3, 4]))
 
         custom_list1 = CustomList([1, 9, 3, 10])
         custom_list2 = CustomList([1, -2, 3, -4])
         custom_list3 = CustomList([2, 7, 6, 6])
-        self.assertEqual((custom_list1 + custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 + custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 9, 3, 10]))
+        self.assertListEqual(custom_list2, CustomList([1, -2, 3, -4]))
 
         custom_list1 = CustomList([1, 9, 3, 10])
         custom_list2 = CustomList([1, -2, 7, -4])
         custom_list1 += custom_list2
         custom_list3 = CustomList([2, 7, 10, 6])
-        self.assertEqual(custom_list1[0:], custom_list3[0:])
+        self.assertListEqual(custom_list1, custom_list3)
+        self.assertListEqual(custom_list2, CustomList([1, -2, 7, -4]))
 
         custom_list1 = CustomList([1, 9, 3, 10])
         list2 = [1, -2, 1.2, -4]
         custom_list3 = CustomList([2, 7, 4.2, 6])
-        self.assertEqual((custom_list1 + list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 + list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 9, 3, 10]))
+        self.assertListEqual(list2, [1, -2, 1.2, -4])
 
         list1 = [1, 9, 3, 10]
         custom_list2 = CustomList([1, -2, 3, -3.7])
         custom_list3 = CustomList([2, 7, 6, 6.3])
-        self.assertEqual((list1 + custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((list1 + custom_list2), custom_list3)
+        self.assertListEqual(list1, [1, 9, 3, 10])
+        self.assertListEqual(custom_list2, CustomList([1, -2, 3, -3.7]))
 
     def test_subtraction(self):
         custom_list1 = CustomList([])
         custom_list2 = CustomList([])
         custom_list3 = CustomList([])
-        self.assertEqual(custom_list1 - custom_list2, custom_list3)
+        self.assertListEqual(custom_list1 - custom_list2, custom_list3)
+        self.assertListEqual(custom_list1, CustomList([]))
+        self.assertListEqual(custom_list2, CustomList([]))
 
         custom_list1 = CustomList([1, 2])
         custom_list2 = CustomList([])
         custom_list3 = CustomList([1, 2])
-        self.assertEqual((custom_list1 - custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 - custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 2]))
+        self.assertListEqual(custom_list2, CustomList([]))
 
         custom_list1 = CustomList([])
         custom_list2 = CustomList([3, 4])
         custom_list3 = CustomList([-3, -4])
-        self.assertEqual((custom_list1 - custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 - custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([]))
+        self.assertListEqual(custom_list2, CustomList([3, 4]))
 
         custom_list1 = CustomList([1, 2, 3, 4])
         custom_list2 = CustomList([1, 9])
         custom_list3 = CustomList([0, -7, 3, 4])
-        self.assertEqual((custom_list1 - custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 - custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 2, 3, 4]))
+        self.assertListEqual(custom_list2, CustomList([1, 9]))
 
         custom_list1 = CustomList([1, 9])
         custom_list2 = CustomList([1, 2, 3, 4])
         custom_list3 = CustomList([0, 7, -3, -4])
-        self.assertEqual((custom_list1 - custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 - custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 9]))
+        self.assertListEqual(custom_list2, CustomList([1, 2, 3, 4]))
 
         custom_list1 = CustomList([1, 9, 3, 10])
         custom_list2 = CustomList([1, -2, 3, -4])
         custom_list3 = CustomList([0, 11, 0, 14])
-        self.assertEqual((custom_list1 - custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 - custom_list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([1, 9, 3, 10]))
+        self.assertListEqual(custom_list2, CustomList([1, -2, 3, -4]))
 
         custom_list1 = CustomList([1, 9, 3, 10])
         custom_list2 = CustomList([5, -2, 1, -4])
         custom_list1 -= custom_list2
         custom_list3 = CustomList([-4, 11, 2, 14])
-        self.assertEqual(custom_list1[0:], custom_list3[0:])
+        self.assertListEqual(custom_list1, custom_list3)
+        self.assertListEqual(custom_list2, CustomList([5, -2, 1, -4]))
 
         custom_list1 = CustomList([2.1, 9, 5.5, 10])
         list2 = [1, -2, 3, -4]
         custom_list3 = CustomList([1.1, 11, 2.5, 14])
-        self.assertEqual((custom_list1 - list2)[0:], custom_list3[0:])
+        self.assertListEqual((custom_list1 - list2), custom_list3)
+        self.assertListEqual(custom_list1, CustomList([2.1, 9, 5.5, 10]))
+        self.assertListEqual(list2, [1, -2, 3, -4])
 
         list1 = [1, 9, 3, 10]
         custom_list2 = CustomList([3, 2.8, 3.5, -4])
         custom_list3 = CustomList([-2, 6.2, -0.5, 14])
-        self.assertEqual((list1 - custom_list2)[0:], custom_list3[0:])
+        self.assertListEqual((list1 - custom_list2), custom_list3)
+        self.assertListEqual(list1, [1, 9, 3, 10])
+        self.assertListEqual(custom_list2, CustomList([3, 2.8, 3.5, -4]))
 
     def test_comparison(self):
         custom_list1 = CustomList([])
