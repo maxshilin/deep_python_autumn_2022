3f0183e228e338498b5ae6ff05a0434b094b2f2c
maxshilin <max.shilin@mail.ru>
2022-11-26 13:25:22 +0300

    Changed ProfileDeco to work with the cProfile

--- 09/profiling.py
+++ 09/profiling.py
@@ -1,39 +1,29 @@
-import time
+import cProfile
+import pstats
 import classes
 
 
 class ProfileDeco:
     def __init__(self, function):
         self.function = function
-        self.counter = 0
-        self.runtimes = []
+        self.prof_stats = None
 
     def __call__(self, *args, **kwargs):
-        start = time.perf_counter()
-        self.function(*args, **kwargs)
-        runtime = time.perf_counter() - start
-        self.counter += 1
-        self.runtimes.append(runtime)
+        with cProfile.Profile() as prof:
+            result = self.function(*args, **kwargs)
+
+        if self.prof_stats is None:
+            self.prof_stats = pstats.Stats(prof)
+        else:
+            self.prof_stats.add(prof)
+
+        return result
 
     def print_stat(self):
-        cumulative = sum(self.runtimes)
-        mean_time = cumulative / self.counter
-        var = (
-            sum(map(lambda x: (mean_time - x) ** 2, self.runtimes))
-            / self.counter
-        ) ** 0.5
-
-        print(
-            f"Number of calls: {self.counter}",
-            f"Cumulative time: {cumulative:.4f} secs",
-            f"Mean time: {mean_time:.4f} secs",
-            f"Variance: {var:.4f} secs",
-            sep="\n",
-        )
+        self.prof_stats.strip_dirs().sort_stats("cumulative").print_stats()
 
     def clear_stat(self):
-        self.counter = 0
-        self.runtimes = []
+        self.prof_stats = None
 
 
 @ProfileDeco
